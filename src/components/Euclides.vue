cc5a0a96c9b340c3321a350da3799df875c7bf31
monroy95 <m.monroyc22@gmail.com>
2021-10-06 21:11:08 -0600

    first commit

---
+++ src/components/Euclides.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Algoritmo de Euclides</h1>
+</template>
+
+<script>
+export default {
+  name: "Euclides",
+};
+</script>
+
+<style lang="css">
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <Euclides />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Euclides from "../components/Euclides.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Euclides,
+  },
+};
+</script>
